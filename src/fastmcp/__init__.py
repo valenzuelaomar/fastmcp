06b291365e11add485167a0910e48ed66d7535b3
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2025-07-19 15:21:08 -0400

    Only configure logging one time (#1187)

--- src/fastmcp/__init__.py
+++ src/fastmcp/__init__.py
@@ -1,10 +1,15 @@
 """FastMCP - An ergonomic MCP interface."""
 
 import warnings
-from importlib.metadata import version
+from importlib.metadata import version as _version
 from fastmcp.settings import Settings
+from fastmcp.utilities.logging import configure_logging as _configure_logging
 
 settings = Settings()
+_configure_logging(
+    level=settings.log_level,
+    enable_rich_tracebacks=settings.enable_rich_tracebacks,
+)
 
 from fastmcp.server.server import FastMCP
 from fastmcp.server.context import Context
@@ -13,7 +18,7 @@ import fastmcp.server
 from fastmcp.client import Client
 from . import client
 
-__version__ = version("fastmcp")
+__version__ = _version("fastmcp")
 
 
 # ensure deprecation warnings are displayed by default
