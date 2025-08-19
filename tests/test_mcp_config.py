9c13ddfd4c68a8ed08733adacda215f5a2fe0607
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2025-08-19 17:24:43 -0400

    Reduce sleep times

--- tests/test_mcp_config.py
+++ tests/test_mcp_config.py
@@ -298,12 +298,12 @@ async def test_multi_client_lifespan(tmp_path: Path):
     with pytest.raises(psutil.NoSuchProcess):
         while True:
             psutil.Process(pid_1)
-            await asyncio.sleep(0.1)
+            await asyncio.sleep(0.01)
 
     with pytest.raises(psutil.NoSuchProcess):
         while True:
             psutil.Process(pid_2)
-            await asyncio.sleep(0.1)
+            await asyncio.sleep(0.01)
 
 
 async def test_multi_client_force_close(tmp_path: Path):
